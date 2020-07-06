b96d6eb18f620186a7931331423f12a5e4be0515
Igor Olhovskiy <IgorOlhovskiy@gmail.com>
2020-07-06 12:48:17 +0300

    ..

--- PBXManager/modules/PBXManager/resources/PBXManagerJS.js
+++ PBXManager/modules/PBXManager/resources/PBXManagerJS.js
@@ -84,26 +84,26 @@ var Vtiger_PBXManager_Js = {
                 </div>`,
             };
         
-    		var settings = {
-				template: 	`<div data-notify="container" class="col-xs-11 col-sm-3 vt-notification vt-notification-{0}" role="alert">
-                        	<div class="notificationHeader">
-                            <span data-notify="icon"></span>
-                            <span data-notify="title">{1}</span>
-                            </div>
-                            <div data-notify="message">{2}</div>
-                            <div class="progress" data-notify="progressbar">
-                            <div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>
-                            </div>
-                            <a href="{3}" target="{4}" data-notify="url"></a>
-                            </div>`,
-				delay: 0,
-				placement: {
-					from: "bottom",
-					align: "right"
-				},
-				offset: 20
-            };
-    		jQuery.notify(options, settings);
+		var settings = {
+			template: 	`<div data-notify="container" class="col-xs-11 col-sm-3 vt-notification vt-notification-{0}" role="alert">
+						<div class="notificationHeader">
+						<span data-notify="icon"></span>
+						<span data-notify="title">{1}</span>
+						</div>
+						<div data-notify="message">{2}</div>
+						<div class="progress" data-notify="progressbar">
+						<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>
+						</div>
+						<a href="{3}" target="{4}" data-notify="url"></a>
+						</div>`,
+			delay: 0,
+			placement: {
+				from: "bottom",
+				align: "right"
+			},
+			offset: 20
+		};
+		jQuery.notify(options, settings);
 
 
                     
